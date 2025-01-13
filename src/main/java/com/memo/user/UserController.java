ac76ce996201c5bf8dc5c952b6847048809c8615
DESKTOP-ID9T9DD\CVI <seyeon021023@gmail.com>
2025-01-13 18:42:30 +0900

    25/01/13 로그인

--- src/main/java/com/memo/user/UserController.java
+++ src/main/java/com/memo/user/UserController.java
@@ -1,5 +1,6 @@
 package com.memo.user;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +23,16 @@ public class UserController {
     @GetMapping("/sign-in-view")
     public String signInView() {
         return "user/signIn";
+    }
+
+    @GetMapping("/sign-out")
+    public String signOut(HttpSession session) {
+        // session을 비운다.
+        session.removeAttribute("userId");
+        session.removeAttribute("userLoginId");
+        session.removeAttribute("userName");
+
+        // 화면 이동
+        return "redirect:/user/sign-in-view";
     }
 }
--- src/main/java/com/memo/user/UserRestController.java
+++ src/main/java/com/memo/user/UserRestController.java
@@ -2,6 +2,8 @@ package com.memo.user;
 
 import com.memo.user.bo.UserBO;
 import com.memo.user.entity.UserEntity;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -56,6 +58,34 @@ public class UserRestController {
         } else {
             result.put("code", 500);
             result.put("error_message", "회원가입에 실패했습니다.");
+        }
+
+        return result;
+    }
+
+    @PostMapping("/sign-in")
+    public Map<String, Object> signIn(
+            @RequestParam("loginId") String loginId,
+            @RequestParam("password") String password,
+            HttpServletRequest request
+    ) {
+        // db select
+        UserEntity user = userBO.getUserEntityByLoginIdPassword(loginId, password);
+
+        // response
+        Map<String, Object> result = new HashMap<>();
+        if (user != null) {
+            // 로그인 성공시 서버에 세션 주머니를 만든다.
+            HttpSession session = request.getSession();
+            session.setAttribute("userId", user.getId());
+            session.setAttribute("userLoginId", user.getLoginId());
+            session.setAttribute("userName", user.getName());
+
+            result.put("code", 200);
+            result.put("result", "성공");
+        } else {
+            result.put("code", 300);
+            result.put("error_message", "존재하지 않는 아이디입니다.");
         }
 
         return result;
